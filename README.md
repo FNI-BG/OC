95f57c34624b13da244e4ed2afaee2d9192d74dc
FNI-BG <poinsettia0207@naver.com>
2021-03-15 21:57:46 +0900

    Add file

---
+++ README.md
@@ -0,0 +1,3 @@
+## Original Character 정리용 웹사이트
+
+**힘들다**
---
+++ js/tab_design.js
@@ -0,0 +1,13 @@
+/* tab_design.js ----------------------------------- */
+jQuery(function($) {
+
+	$('#tab_design').tabs({
+		start_index: 1,
+		random: false,
+		transition_time: 200
+	});
+	
+	
+	
+});
+
